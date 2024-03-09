908daa63c9314b1c92dc1047fc0e479934d548b7
Kira7124 <afc2015@naver.com>
2024-03-09 09:27:38 +0900

    첫커밋

---
+++ src/main/java/com/project3/yogiaudio/product/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.project3.yogiaudio.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/product")
+public class ProductController {
+	
+	// http://localhost:80/product/main
+	
+	@GetMapping("/main")
+	public String productMainGET() {
+		log.debug("메인페이지호출테스트");
+		return"product/main";
+	}
+	
+}
---
+++ src/main/java/com/project3/yogiaudio/product/dto/ProductDTO.java
@@ -0,0 +1,5 @@
+package com.project3.yogiaudio.product.dto;
+
+public class ProductDTO {
+
+}
