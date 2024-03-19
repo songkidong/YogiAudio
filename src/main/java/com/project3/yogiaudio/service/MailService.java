e70e592bd1ba8f3eb84631d105a73443e6d766c7
songkidong <ehdzl5464@naver.com>
2024-03-19 20:47:24 +0900

    이메일인증 구현, 소셜로그인 수정, 회원가입 유효성검사 추가

--- src/main/java/com/project3/yogiaudio/service/MailService.java
+++ src/main/java/com/project3/yogiaudio/service/MailService.java
@@ -1,68 +1,54 @@
 package com.project3.yogiaudio.service;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-@Slf4j
 @Service
 @Transactional
-@RequiredArgsConstructor
 public class MailService {
 
 	@Autowired
 	private JavaMailSender javaMailSender;
 
 	// 인증 번호
-	private static String generatedCode;
+	private String generatedCode = tempPassword();
 
-	// 메일 발송
-	public void sendCodeByEmail(String receiver) throws Exception {
-		// 인증코드 생성
-		int code = ThreadLocalRandom.current().nextInt(100000, 1000000); // 랜덤 생성
-		generatedCode = "" + code;
+	// 메일 보내는 메소드
+	public void mailSend(String email) {
 
-		String title = "YogiAudio 인증코드";
-		String sender = "YogiAudio";
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(email);
+		message.setSubject("YogiAudio");
+		message.setText("YogiAudio 인증번호 : " + generatedCode);
+		message.setFrom("ehdzl5464@naver.com");
+		message.setReplyTo("ehdzl5464@naver.com");
+		javaMailSender.send(message);
+	}
 
-		MimeMessage message = javaMailSender.createMimeMessage();
-
-		message.addRecipients(MimeMessage.RecipientType.TO, receiver); // 받은 이
-		message.setSubject(title); // 제목 설정
-		message.setFrom(sender); // 보내는 사람 설정
-
-		// 메일 내용 설정
-		String msgOfEmail = "";
-		msgOfEmail += "<div style='margin:20px;'>";
-		msgOfEmail += "<br>";
-		msgOfEmail += "<div  align='center' style='border:1px solid #ccc; border-radius: 5px;  ";
-		msgOfEmail += "box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);';>";
-		msgOfEmail += "<h3 style='color: #06BBCC;'>YogiAudio 인증 코드</h3>";
-		msgOfEmail += "<div style='font-size:130%'>";
-		msgOfEmail += "<strong>" + generatedCode + "<strong/><div><br/> ";
-		msgOfEmail += "</div>";
-
-		message.setText(msgOfEmail, "utf-8", "html");
-
-		javaMailSender.send(message); // 전송
-
+	// 랜덤함수로 인증번호 만들기
+	public String tempPassword() {
+		char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+				'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+		String password = "";
+		// 문자 배열 길이의 값을 랜덤으로 10개를 뽑아 구문을 작성함
+		int j = 0;
+		for (int i = 0; i < 10; i++) {
+			j = (int) (charSet.length * Math.random());
+			password += charSet[j];
+		}
+		return password;
 	}
 
 	// 인증 번호 확인
-	public Boolean confirmCodeByEmail(String code) {
+	public Boolean confirmCodeByEmail(String number) {
 
-		if (code.equals(generatedCode)) {
+		if (number.equals(generatedCode)) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-
 }
