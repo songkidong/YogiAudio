447f7e2a881e4d108c035255d151597389701349
z82714567 <z82714567@gmail.com>
2024-03-28 11:56:36 +0900

    board 주석

--- src/main/java/com/project3/yogiaudio/repository/interfaces/board/FreeCommentRepository.java
+++ src/main/java/com/project3/yogiaudio/repository/interfaces/board/FreeCommentRepository.java
@@ -12,14 +12,19 @@ import com.project3.yogiaudio.repository.entity.board.BoardFreeComment;
 @Mapper
 public interface FreeCommentRepository {
 
+	// 댓글 상세보기
 	public BoardFreeComment freeCommentView(int boardFreeId);
 
+	// 댓글 리스트
 	public List<BoardFreeComment> selectFreeCommentList(int boardFreeId);
 
+	// 댓글 작성하기
 	public int freeCommentSave(BoardFreeComment boardFreeComment);
 
+	// 댓글 삭제하기
 	public int freeCommentDelete(int id);
 
+	// 댓글 수정하기
 	public int freeCommentUpdate(Map<String, Object> params);
 
 }
